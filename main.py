e3a9a23828a0a8d20ab7ff2c79160203e240d0b0
Melissa Schrum <melissaschrum@gmail.com>
2016-08-23 20:46:08 -0500

    First commit, working welcome page

--- main.py
+++ main.py
@@ -1,25 +1,137 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 import webapp2
+import cgi
+import re
 
-class MainHandler(webapp2.RequestHandler):
+page_header = """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Signup</title>
+    <style type="text/css">
+            .error {
+                color: red;
+            }
+    </style>
+</head>
+<body>
+    <h1>Signup</h1>
+"""
+
+# html boilerplate for the bottom of every page
+page_footer = """
+</body>
+</html>
+"""
+SignUp_form = """
+<form action="/" method="post">
+    <table>
+        <tbody>
+            <tr>
+                <td>
+                    <label>User Name:</label>
+                </td>
+                <td>
+                    <input type="text" name="username"/>
+                    <span class="error"></span>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label>Password:</label>
+                </td>
+                <td>
+                    <input type="password" name="password"/>
+                    <span class="error"></span>
+                </td>
+            </tr>
+                <td>
+                    <label>Verify Password:</label>
+                </td>
+                <td>
+                    <input type="password" name="verify"/>
+                    <span class="error"></span>
+                </td>
+            </tr>
+                <td>
+                    <label>E-mail Address:</label>
+                </td>
+                <td>
+                    <input type="text" name="email"/>
+                    <span class="error"></span>
+                </td>
+            </tr>
+                <td>
+                    <input type="submit" value="Submit"/>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</form>
+"""
+
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def valid_username(username):
+    return username and USER_RE.match(username)
+PASS_RE = re.compile(r"^.{3,20}$")
+def valid_password(password):
+    return password and PASS_RE.match(password)
+EMAIL_RE = re.compile(r"^[\S]+@[\S]+.[\S]+$" )
+def valid_email(email):
+    return not email or EMAIL_RE.match(email)
+
+class Index(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        response = page_header + SignUp_form + page_footer
+        self.response.write(response)
+
+    def post(self):
+        have_error = False
+        username = self.request.get("username")
+        password = self.request.get("password")
+        verify = self.request.get("verify")
+        email = self.request.get("email")
+
+        params = dict(username = username, email = email)
+
+        if not valid_username(username):
+            params["error_username"] = "That's not a valid Username."
+            have_error = True
+        if not valid_password(password):
+            params["error_password"] = "That's not a valid password."
+            have_error = True
+        elif password != verify:
+            params["error_verify"] = "Your passwords didn't match."
+            have_error = True
+        if not valid_email(email):
+            params["error_email"] = "That's not a valid e-mail"
+            have_error = True
+        if have_error:
+            response = page_header + SignUp_form + page_footer
+            self.response.write(response)
+        else:
+            self.redirect("/welcome?username=" + username)
+class Welcome(webapp2.RequestHandler):
+    def get(self):
+        username = self.request.get("username")
+        if valid_username(username):
+            response = """
+            <!DOCTYPE html>
+            <html>
+            <head>
+                <title>Signup</title>
+            </head>
+            <body>
+                <h2>Welome {0}</h2>
+            </body>
+            </head>
+            </html>
+            """
+            self.response.write(response.format(username))
+        else:
+            self.redirect('/')
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', Index),
+    ('/welcome', Welcome)
 ], debug=True)
